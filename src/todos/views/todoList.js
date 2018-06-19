d782e8d0928f9a260e3c242c2c9d36c54f8e7c9e
bojan <liuboyang276080914@gmail.com>
2018-06-19 17:10:17 +0800

    list item 增加 FILP 过渡动画

--- src/todos/views/todoList.js
+++ src/todos/views/todoList.js
@@ -3,24 +3,45 @@ import { connect } from 'react-redux'
 import { List } from 'antd'
 import TodoItem from './todoItem'
 import { selectVisibleTodos } from '../selector'
+import FlipMove from 'react-flip-move'
 
-const TodoList = ({todos}) => {
-    return (
-      <List>
-        {
-          todos.length > 0 && todos.map(item => (
-            <TodoItem 
-              key={item.id}
-              id={item.id}
-              text={item.text}
-              completed={item.completed}
-              typeId={item.typeId}
-            />
-          ))
-        }
-      </List>
-    )
+const _animeConfig = {
+  duration: 250,
+  delay: 0,
+  easingPreset: { value: '0.25,0.1,0.25,1.0',   label: 'ease' },
+  easingValues: ['0.25','0.1','0.25','1.0'],
+  staggerDelayBy: 20,
+  staggerDurationBy: 15,
+  preset: 0
 }
+
+const _formatEasing = () => `cubic-bezier(${ _animeConfig.easingValues.join(',') })`
+
+const TodoList = ({todos}) => (
+  <List>
+    <FlipMove 
+      enterAnimation='accordionHorizontal'
+      leaveAnimation={false}
+      duration={_animeConfig.duration}
+      delay={_animeConfig.delay}
+      staggerDelayBy={_animeConfig.staggerDelayBy}
+      staggerDurationBy={_animeConfig.staggerDurationBy}
+      easing={_formatEasing()}
+    >
+      {
+        todos.length > 0 && todos.map(item => (
+          <TodoItem 
+            key={item.id}
+            id={item.id}
+            text={item.text}
+            completed={item.completed}
+            typeId={item.typeId}
+          />
+        ))
+      }
+    </FlipMove>
+  </List>
+)
 
 const mapStateToProps = state => {
   return {
