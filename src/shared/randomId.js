6b9bfa6d38b0190386799e09f95da11c8f68af2b
bojan <liuboyang276080914@gmail.com>
2018-06-20 14:10:14 +0800

    将 item id 改为 randomid

---
+++ src/shared/randomId.js
@@ -0,0 +1,6 @@
+import nanoid from 'nanoid';
+
+const length = 6;
+const randomId = () => nanoid(length);
+
+export { randomId };
--- src/todos/actions.js
+++ src/todos/actions.js
@@ -1,26 +1,30 @@
-import { ADD_TODO, TOGGLE_TODO, REMOVE_TODO, CHANGE_TODO_TAG } from './actionTypes'
-
-let nextTodoId = 0;
+import {
+  ADD_TODO,
+  TOGGLE_TODO,
+  REMOVE_TODO,
+  CHANGE_TODO_TAG
+} from './actionTypes';
+import { randomId } from '../shared/randomId';
 
 export const addTodo = text => ({
   type: ADD_TODO,
   completed: false,
-  id: nextTodoId++,
+  id: randomId(),
   text: text
-})
+});
 
 export const toggleTodo = id => ({
   type: TOGGLE_TODO,
   id: id
-})
+});
 
 export const removeTodo = id => ({
   type: REMOVE_TODO,
   id: id
-})
+});
 
 export const changeTag = (id, typeId) => ({
   type: CHANGE_TODO_TAG,
   id: id,
   typeId: typeId
-})
+});
